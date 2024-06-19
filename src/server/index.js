1a87e819b92627a83c830c9ab2cc460b5272a57c
SergeyEcoil <ecoil@bk.ru>
2024-06-20 00:06:20 +0400

    "HMR"

--- src/server/index.js
+++ src/server/index.js
@@ -1,19 +1,43 @@
-import app from "./app";
-import { Server as WebsocketServer } from "socket.io";
+import express from "express";
+import path from "path";
+import cors from "cors";
 import http from "http";
+import { Server as WebsocketServer } from "socket.io";
+import webpack from "webpack";
+import webpackDevMiddleware from "webpack-dev-middleware";
+import webpackHotMiddleware from "webpack-hot-middleware";
+import webpackConfig from "../../webpack.config";
 import { connectDB } from "./db";
 import sockets from "./sockets";
 import { PORT } from "./config";
 
+const app = express();
+const compiler = webpack(webpackConfig);
+
+app.use(cors());
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+  })
+);
+app.use(webpackHotMiddleware(compiler));
+
+app.use(express.static(path.join(__dirname, "../../public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public", "index.html"));
+});
+
 connectDB();
 
 const server = http.createServer(app);
-const httpServer = server.listen(PORT);
-console.log("Server is running on port", PORT);
+const httpServer = server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 const io = new WebsocketServer(httpServer, {
   cors: {
-    origin: "*", // Разрешить запросы с любого домена
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
